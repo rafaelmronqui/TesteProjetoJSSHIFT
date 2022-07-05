63860b15d6d88ae95d61ded4c71045b56eb30609
rafaelmronqui <rafaelmronqui@gmail.com>
2022-07-04 22:05:21 -0300

    Envio um

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+﻿function validarFaculdade()
+{
+	var verificar = prompt("Digite uma opção:");
+
+	switch (verificar)
+	{
+		case "1":
+			var recebeGraduacao = prompt("Digite a graduação que deseja fazer:");
+			alert("Parabéns, você está cadastrado para cursar a graduação em " + recebeGraduacao);
+			break;
+		case "2":
+			var graduacao = prompt("Digite o nome do curso que você é graduado:");
+			var posgraduacao = prompt("Digite o nome da pós-graduação que deseja fazer:");
+			alert("Parabéns, você já é graduado em " + graduacao + " e agora irá cursar pós-graduação em: " + posgraduacao);
+			break;
+		default:
+			alert("Digitou uma opção errada.");
+			break;
+	}
+}
+
